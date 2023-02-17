9267c629af49f1184193c6835196e689d0da36a8
Harshalekkala <105717718+lekkalaharsha@users.noreply.github.com>
2023-02-18 01:40:21 +0530

    js,app

--- code-pen/src/components/App.js
+++ code-pen/src/components/App.js
@@ -1,28 +1,57 @@
-import React from "react";
-import Editor from "./editor";
+import React,{useState}from "react";
+import Editor from "./Editor";
 function App() {
+  const [html,setHtml] = useState('')
+  const [js,setJs] = useState('')
+  const [css,setCss] = useState('')
   return  (
-    <>
+  <>
     <div className="pane top-pane">
-    <Editor />
-    <Editor />
-    <Editor />
-    <div className="pane">
+    <Editor 
+    language='xmal'
+    displayName='HTML'
+    value={html}
+    onchage={setHtml}
 
+    
+    />
+<Editor 
+    language='css'
+    displayName='CSS'
+    value={css}
+    onchage={setCss}
+ 
+
+    />
+<Editor 
+    language='javascript'
+    displayName='js'
+    value={js}
+    onchage={setJs}
+ 
+
+    />
+
+   
+    
+    <div className="pane">
     <iframe
 
-    title="output"
-    sandbox="allow-scripts"
-    frameBoder="0"
-    width="100%"
-    height="100%"
+     title="output"
+     sandbox="allow-script"
+     frameBoder="0"
+     width="100%"
+     height="100%"
     />
+    
       
+
+    
     </div>
 
       
     </div>
-    </>
+  </>
    
   )
 }
--- code-pen/src/components/editor.js
+++ code-pen/src/components/editor.js
@@ -1,9 +1,45 @@
 import React from 'react'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/material.css'
+import 'codemirror/mode/xml/xml'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/css/css'
+import {Controlled as ControlledEditor} from 'react-codemirror2'
 
-export default function Editor() {
+export default function Editor(props) {
+  const {
+    language,
+    displayName,
+    value,
+    onchage
+
+  }=props
+  function handleChange (editor,data,value){
+    onchage(value)
+  }
   return (
-    <div>
-    harsha
+    <div className='editor-container'>
+    <div className='editor-title'>
+    {displayName}
+    <button>o/c</button>
+    </div>
+    <ControlledEditor 
+      onBeforeChange={handleChange}
+      value={value}
+      className="code-mirror-wrapper"
+      options={{
+        lineWrapping:true,
+        lint:true,
+        mode:language,
+        theme:"material",
+        lineNumbers:1
+        
+
+      }}
+
+
+    />
+    
       
     </div>
   )
