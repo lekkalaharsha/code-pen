d76d96d6059a13dbf08a2e5a031f2728f0d02b89
Harshalekkala <105717718+lekkalaharsha@users.noreply.github.com>
2023-02-18 03:01:41 +0530

    code-pen

--- code-pen/src/components/App.js
+++ code-pen/src/components/App.js
@@ -1,58 +1,68 @@
-import React,{useState}from "react";
-import Editor from "./Editor";
+import React, { useState, useEffect } from 'react';
+import Editor from './Editor'
+import useLocalStorage from '../hooks/useLocalStorage'
+
 function App() {
-  const [html,setHtml] = useState('')
-  const [js,setJs] = useState('')
-  const [css,setCss] = useState('')
-  return  (
-  <>
-    <div className="pane top-pane">
-    <Editor 
-    language='xmal'
-    displayName='HTML'
-    value={html}
-    onchage={setHtml}
+  const [html, setHtml] = useLocalStorage('html', '')
+  const [css, setCss] = useLocalStorage('css', '')
+  const [js, setJs] = useLocalStorage('js', '')
+  const [python, setPython] = useLocalStorage('js', '')
+  const [srcDoc, setSrcDoc] = useState('')
 
-    
-    />
-<Editor 
-    language='css'
-    displayName='CSS'
-    value={css}
-    onchage={setCss}
- 
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
+          <pyscript>${python} </pyscript>
+        </html>
+      `)
+    }, 250)
 
-    />
-<Editor 
-    language='javascript'
-    displayName='js'
-    value={js}
-    onchage={setJs}
- 
+    return () => clearTimeout(timeout)
+  }, [html, css, js,python])
 
-    />
-
-   
-    
-    <div className="pane">
-    <iframe
-
-     title="output"
-     sandbox="allow-script"
-     frameBoder="0"
-     width="100%"
-     height="100%"
-    />
-    
-      
-
-    
-    </div>
-
-      
-    </div>
-  </>
-   
+  return (
+    <>
+      <div className="pane top-pane">
+        <Editor
+          language="pytho"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          language="javascript"
+          displayName="JS"
+          value={js}
+          onChange={setJs}
+        />
+          <Editor
+          language="python"
+          displayName="python"
+          value={python}
+          onChange={setPython}
+        />
+      </div>
+      <div className="pane">
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </>
   )
 }
 
